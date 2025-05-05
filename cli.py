1485f4298b5af93df06a6acc77a3cf723ee59b6f
haturatu <taro@eyes4you.org>
2025-05-06 00:41:00 +0900

    fix: mikuではなかった場合の戻り値1を返す

--- cli.py
+++ cli.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 from model.predict import load_model, predict
 from utils.download import download_image
@@ -14,11 +15,15 @@ def main():
     pred, conf = predict(model, image)
 
     if pred is None:
-        print("failed")
+        print("Prediction failed.", file=sys.stderr)
+        sys.exit(255)
+    elif pred != 0:
+        label = "Miku isn't in a picture."
+        print(f"{label} (Confidence: {conf:.2f})", file=sys.stderr)
+        sys.exit(1)
     else:
-        label = "Miku is in a picture!" if pred == 0 else "Miku isn't in a picture."
+        label = "Miku is in a picture!"
         print(f"{label} (Confidence: {conf:.2f})")
-
 
 if __name__ == '__main__':
     main()
